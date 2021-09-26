16851f080879c168d213909f45f472ce1c2ff693
Rasedul <raselr699@gmail.com>
2021-09-26 11:22:45 +0600

    header section

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className="header-main">
+            <h1>MAKE SURE RIGHT AUTO SERVICE</h1>
+            <p>Car repairs and maintenance can be expensive and no one wants to have pay to repair damage caused by shoddy repair service. When you bring your car into our auto shop, you won’t have to worry because our staff is comprised of ASE certified technicians who are committed to making sure you have a safe dependable car.</p>
+            <h2>Our all over services budget: $3000</h2>
+        </div>
+    );
+};
+
+export default Header;
